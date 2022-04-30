a20aeb36440839adff7a843f64518768971f79e6
kidosol3856 <54834225+kidosol3856@users.noreply.github.com>
2022-04-30 15:53:28 +0800

    Update GameState.js

--- Jitendra - Space hipster/js/state/GameState.js
+++ Jitendra - Space hipster/js/state/GameState.js
@@ -3,16 +3,59 @@ var SpaceHipster = SpaceHipster || {};
 SpaceHipster.GameState = {
     //Initiate game settings
     init: function () {
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.PLAYER_SPEED = 200;
+        this.BULLET_SPEED = -1000;
 
     },
 
     //Load the game assets before the game starts
     preload: function() {
-        this.load.image('space', 'assets/images/space.png');
-        this.load.image('player', 'assets/images/player.png');
-        this.load.image('bullet', 'assets/images/bullet.png');
-        this.load.image('enemyParticle', 'assets/images/enemyParticle.png');
-        this.load.spritesheet('yellowEnemy', 'assets/images/yellow_enemy.png', 50, 46, 3, 1, 1);
+        this.load.image('space', 'images/space.png');
+        this.load.image('player', 'images/player.png');
+        this.load.image('bullet', 'images/bullet.png');
+        this.load.image('enemyParticle', 'images/enemyParticle.png');
+        this.load.spritesheet('yellowEnemy', 'images/yellow_enemy.png', 50, 46, 3, 1, 1);
+        this.load.spritesheet('redEnemy', 'images/red_enemy.png', 50, 46, 3, 1, 1);
+        this.load.spritesheet('greenEnemy', 'images/green_enemy.png', 50, 46, 3, 1, 1);
+    },
+
+    //executed after everything is loaded
+    create: function () {
+        this.background = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'space');
+
+        this.background.autoScroll(0, 30);
+
+        //player
+        this.player = this.add.sprite(this.game.world.centerX, this.game.world.height - 100, 'player');
+        this.player.anchor.setTo(0.5);
+        this.game.physics.arcade.enable(this.player);
+        this.player.body.collideWorldBounds = true;
+
+        this.initBullets();
+        this.shootingTimer = this.game.timer.events.loop
+
+    },
+    
+    update: function () {
+        this.player.body.velocity.x = 0;
+
+        if(this.game.input.activePointer.isDown) {
+            var targetX = this.game.input.activePointer.position.x;
+
+            var direction = targetX >= this.game.world.centerX ? 1 : -1;
+
+            this.player.body.velocity.x = direction * this.PLAYER_SPEED;
+        }
+    },
+
+    initBullets: function () {
+        this.playerBullets = this.add.group();
+        this.playerBullets.enableBody = true;
     }
+
+
 }
