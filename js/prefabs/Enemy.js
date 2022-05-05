53fc606a2b0245120adc43c9d802f27bac92669c
kidosol3856 <54834225+kidosol3856@users.noreply.github.com>
2022-05-05 16:33:52 +0800

    Update Enemy.js

--- js/prefabs/Enemy.js
+++ js/prefabs/Enemy.js
@@ -13,6 +13,11 @@ SpaceHipster.Enemy = function(game, x, y, key, health, enemyBullets) {
     this.health = health;
 
     this.enemyBullets = enemyBullets;
+
+    this.enemyTimer = this.game.time.create(false);
+    this.enemyTimer.start();
+
+    this.scheduleShooting();
 };
 
 SpaceHipster.Enemy.prototype = Object.create(Phaser.Sprite.prototype);
@@ -35,7 +40,52 @@ SpaceHipster.Enemy.prototype.update = function () {
 };
 
 SpaceHipster.Enemy.prototype.damage = function(amount) {
+
     Phaser.Sprite.prototype.damage.call(this, amount);
 
     this.play('getHit');
+
+    //particle explosion
+    if(this.health <= 0) {
+        var emitter = this.game.add.emitter(this.x, this.y, 100);
+        emitter.makeParticles('enemyParticle');
+        emitter.minParticleSpeed.setTo(-200, -200);
+        emitter.maxParticleSpeed.setTo(200, 200);
+        emitter.gravity = 0;
+        emitter.start(true, 500, null, 100);
+        
+        this.enemyTimer.pause();
+    }
 };
+
+    SpaceHipster.Enemy.prototype.reset = function(x, y, health, key, scale, speedX, speedY) {
+        Phaser.Sprite.prototype.reset.call(this, x, y, health);
+
+        this.loadTexture(key);
+        this.scale.setTo(scale);
+        this.body.velocity.x = speedX;
+        this.body.velocity.y = speedY;
+
+        this.enemyTimer.resume();
+
+    };
+
+    SpaceHipster.Enemy.prototype.scheduleShooting = function() {
+      this.shoot();
+      
+      this.enemyTimer.add(Phaser.Timer.SECOND * 2, this.scheduleShooting, this);
+    };
+
+    SpaceHipster.Enemy.prototype.shoot = function() {
+        var bullet = this.enemyBullets.getFirstExists(false);
+
+        if(!bullet) {
+            bullet = new SpaceHipster.EnemyBullet(this.game, this.x, this.bottom);
+            this.enemyBullets.add(bullet);
+        }
+        else {
+            bullet.reset(this.x, this.y);
+        }
+
+        bullet.body.velocity.y = 100;
+    };
